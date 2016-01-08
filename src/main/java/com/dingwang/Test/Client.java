4eb0fde51b94c0614bf71f3ec86f7fc5072ae851
dingwang <wangding_91@163.com>
2016-01-08 10:28:19 +0800

    maven打包学习

--- src/main/java/com/dingwang/Test/Client.java
+++ src/main/java/com/dingwang/Test/Client.java
@@ -7,13 +7,16 @@
  */
 package com.dingwang.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.dingwang.dao.PolicyDTOMapper;
-import com.dingwang.model.PolicyDTO;
+import com.taobao.diamond.manager.ManagerListenerAdapter;
+import com.taobao.diamond.manager.impl.DefaultDiamondManager;
 
 /**
  * 类Client.java的实现描述：
@@ -26,11 +29,11 @@ public class Client {
 
     @SuppressWarnings("deprecation")
     public static void main(String[] args) throws Exception {
-        ApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "classpath:applicationContext.xml" });
-        PolicyDTOMapper policyDAO = (PolicyDTOMapper) context.getBean("policyDAO");
-        PolicyDTO policyDTO = policyDAO.selectByPrimaryKey(7L);
-        System.out.println("############" + policyDTO.getChannelCode() + "########");
+        //        ApplicationContext context = new ClassPathXmlApplicationContext(
+        //                new String[] { "classpath:applicationContext.xml" });
+        //        PolicyDTOMapper policyDAO = (PolicyDTOMapper) context.getBean("policyDAO");
+        //        PolicyDTO policyDTO = policyDAO.selectByPrimaryKey(7L);
+        //        System.out.println("############" + policyDTO.getChannelCode() + "########");
         //        HelloWorldService hw = (HelloWorldService) context.getBean("proxyHW");
         //        log.info("-----------------华丽的分割线-------------------------");
         //        hw.sayHelloWorld("LiuBei");
@@ -44,24 +47,63 @@ public class Client {
         //        ClassLoader cl = new Client().getClass().getClassLoader();
         //        final GroovyClassLoader groovyCl = new GroovyClassLoader(cl);
         //
-        //        new DefaultDiamondManager("DEFAULT_GROUP", "com.taobao.groovycode.test", new ManagerListenerAdapter() {
+        getString();
         //
-        //            public void receiveConfigInfo(String configInfo) {
-        //                log.info("########configInfo=" + configInfo);
-        //                Class groovyClass = groovyCl.parseClass(configInfo);
-        //                try {
-        //                    Groovy groovy = (Groovy) groovyClass.newInstance();
-        //                    groovy.logGroovy(context);
-        //                } catch (InstantiationException e) {
-        //                    // TODO Auto-generated catch block
-        //                    e.printStackTrace();
-        //                } catch (IllegalAccessException e) {
-        //                    // TODO Auto-generated catch block
-        //                    e.printStackTrace();
-        //                }
-        //            }
-        //        }).getAvailableConfigureInfomation(100000);
-        //
-        //        Thread.currentThread().sleep(10000);
+        System.out.println("1111111111111111111111111111111111");
+        Thread.currentThread().sleep(10000);
+    }
+
+    public static Properties String2Properties(String data) {
+        Properties p = null;
+        if (StringUtils.isNotBlank(data)) {
+            p = new Properties();
+            try {
+                p.load(new ByteArrayInputStream(data.getBytes()));
+                return p;
+            } catch (IOException e) {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    private static Properties properties;
+
+    @SuppressWarnings("deprecation")
+    public static String getString() {
+        return new DefaultDiamondManager("DEFAULT_GROUP", "com.taobao.groovycode.test", new ManagerListenerAdapter() {
+
+            public void receiveConfigInfo(String configInfo) {
+                log.info("########configInfo=" + configInfo);
+                //                Class groovyClass = groovyCl.parseClass(configInfo);
+                //                try {
+                //                    Groovy groovy = (Groovy) groovyClass.newInstance();
+                //                    groovy.logGroovy(context);
+                //                } catch (InstantiationException e) {
+                //                    // TODO Auto-generated catch block
+                //                    e.printStackTrace();
+                //                } catch (IllegalAccessException e) {
+                //                    // TODO Auto-generated catch block
+                //                    e.printStackTrace();
+                //                }
+                //            }
+                log.info("########configInfo=" + configInfo);
+                properties = String2Properties(configInfo);
+                StringBuilder key = new StringBuilder();
+                key.append("property_").append(14).append("_").append(6040002);
+                String property = (String) properties.get(key.toString());
+                if (StringUtils.isNotBlank(property)) {
+                    String[] propertyArray = property.split(",");
+                    if (propertyArray.length < 4) {
+                        throw new IllegalArgumentException("the size of the propertyArray less than 4! lack property!");
+                    } else {
+                        System.out.println("##############property=" + property);
+                        System.out.println(propertyArray[0] + "######" + propertyArray[1] + "######" + propertyArray[2]
+                                + "######" + propertyArray[3] + "######");
+                    }
+                }
+            }
+        }).getAvailableConfigureInfomation(100000);
     }
 }
